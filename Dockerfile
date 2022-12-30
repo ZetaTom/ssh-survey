8df2ed368b2717d3a28f5ad47051ec552fdf1a7a
ZetaTom <70907959+ZetaTom@users.noreply.github.com>
2022-12-30 17:55:53 +0100

    update to last state

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM alpine:3.16.0
 
-COPY --chown=0:0 src /opt
-
 RUN apk add --no-cache --update dropbear nano uuidgen \
     && adduser -D -s /bin/sh survey \
     && mkdir -p /etc/dropbear
---
+++ opt/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+date=$(date +%FT%H%M%S)
+homedir=/home/survey/${date}
+passwd=$(tr -cd '[:alnum:]' < /dev/random | fold -w 12 | head -n 1)
+echo "survey:${passwd}"
+echo "survey:${passwd}" | chpasswd
+mkdir -p ${homedir}
+test -f /opt/template && cp -v /opt/template ${homedir}/template
+test -f /opt/deadline && deadline=$(cat /opt/deadline | tr -cd [:digit:]) && echo -n "Current deadline: " && date --date "@${deadline}"
+chown survey:survey ${homedir}
+if [ ! -f /opt/dropbear_ecdsa_host_key ]; then
+	dropbearkey  -t ecdsa -s 521 -f /etc/dropbear/dropbear_ecdsa_host_key
+else
+	ln -s /opt/dropbear_ecdsa_host_key /etc/dropbear/dropbear_ecdsa_host_key
+fi
+dropbear -F -E -w -T 2 -j -k -G survey -c "sh /opt/survey.sh ${homedir} ${deadli
